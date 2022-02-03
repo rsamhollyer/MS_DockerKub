4e815bc567d9a400854a153afcd3d15c8147677a
Sam Hollyer <rsamhollyer@gmail.com>
2022-02-03 17:33:15 -0600

    add dockerfile

--- data-volumes-01-starting-setup/server.js
+++ data-volumes-01-starting-setup/server.js
@@ -1,5 +1,5 @@
 const fs = require('fs').promises;
-const exists = require('fs').exists;
+const { exists } = require('fs');
 const path = require('path');
 
 const express = require('express');
@@ -13,33 +13,33 @@ app.use(express.static('public'));
 app.use('/feedback', express.static('feedback'));
 
 app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'pages', 'feedback.html');
-  res.sendFile(filePath);
+    const filePath = path.join(__dirname, 'pages', 'feedback.html');
+    res.sendFile(filePath);
 });
 
 app.get('/exists', (req, res) => {
-  const filePath = path.join(__dirname, 'pages', 'exists.html');
-  res.sendFile(filePath);
+    const filePath = path.join(__dirname, 'pages', 'exists.html');
+    res.sendFile(filePath);
 });
 
 app.post('/create', async (req, res) => {
-  const title = req.body.title;
-  const content = req.body.text;
+    const { title } = req.body;
+    const content = req.body.text;
 
-  const adjTitle = title.toLowerCase();
+    const adjTitle = title.toLowerCase();
 
-  const tempFilePath = path.join(__dirname, 'temp', adjTitle + '.txt');
-  const finalFilePath = path.join(__dirname, 'feedback', adjTitle + '.txt');
+    const tempFilePath = path.join(__dirname, 'temp', `${adjTitle}.txt`);
+    const finalFilePath = path.join(__dirname, 'feedback', `${adjTitle}.txt`);
 
-  await fs.writeFile(tempFilePath, content);
-  exists(finalFilePath, async (exists) => {
-    if (exists) {
-      res.redirect('/exists');
-    } else {
-      await fs.rename(tempFilePath, finalFilePath);
-      res.redirect('/');
-    }
-  });
+    await fs.writeFile(tempFilePath, content);
+    exists(finalFilePath, async exists => {
+        if (exists) {
+            res.redirect('/exists');
+        } else {
+            await fs.rename(tempFilePath, finalFilePath);
+            res.redirect('/');
+        }
+    });
 });
 
 app.listen(80);
