e3e28a8411341dcecf9724b50b51212ac6cbc90f
Sam Hollyer <rsamhollyer@gmail.com>
2022-02-05 10:33:22 -0600

    initial push

--- networks/models/favorite.js
+++ networks/models/favorite.js
@@ -1,10 +1,10 @@
 const { Schema, model } = require('mongoose');
 
 const favoriteSchema = new Schema({
-  type: String, // 'movie' | 'character'
-  name: String,
-  url: String
-}); 
+    type: String, // 'movie' | 'character'
+    name: String,
+    url: String,
+});
 
 const Favorite = model('Favorite', favoriteSchema);
 
