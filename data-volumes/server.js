83d2770c933e9651073412332cd19a56cd390a3c
Sam Hollyer <rsamhollyer@gmail.com>
2022-02-05 07:07:33 -0600

    stp[

--- data-volumes/server.js
+++ data-volumes/server.js
@@ -31,9 +31,11 @@ app.post('/create', async (req, res) => {
     const tempFilePath = path.join(__dirname, 'temp', `${adjTitle}.txt`);
     const finalFilePath = path.join(__dirname, 'feedback', `${adjTitle}.txt`);
 
+    console.log('TEST!!!!!');
+
     await fs.writeFile(tempFilePath, content);
-    exists(finalFilePath, async exist => {
-        if (exist) {
+    exists(finalFilePath, async exists => {
+        if (exists) {
             res.redirect('/exists');
         } else {
             await fs.copyFile(tempFilePath, finalFilePath);
@@ -43,4 +45,4 @@ app.post('/create', async (req, res) => {
     });
 });
 
-app.listen(80);
+app.listen(process.env.PORT);
