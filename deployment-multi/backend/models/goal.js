941671dfa98bdc902cbf1bac460900ca0f92a1f3
Sam Hollyer <rsamhollyer@gmail.com>
2022-03-12 03:31:13 -0600

    updates

--- deployment-multi/backend/models/goal.js
+++ deployment-multi/backend/models/goal.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const { Schema } = mongoose;
+const Schema = mongoose.Schema;
 
 const goalSchema = new Schema({
-    text: String,
+  text: String
 });
 
 const GoalModel = mongoose.model('Goal', goalSchema);
